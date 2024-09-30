3d73c1b0c90117ea1141ec1936e9beed551e45c4
Matheus Marciano <undefined>
2024-09-30 12:20:18 -0300

    ✨ Add division feature

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 import sys
 from src.add import add
 from src.subtraction import subtraction
+from src.division import division
 
 if __name__ == '__main__':
     a, op, b = sys.argv[1].split(' ')
@@ -9,5 +10,7 @@ if __name__ == '__main__':
         result = add(a, b)
     elif op == '-':
         result = subtraction(a, b)
+    elif op == '/':
+        result = division(a, b)
     
     print(result)
