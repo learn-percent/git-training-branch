fa685225bf50aa0ccc257226956dc82b59122eb9
Matheus Marciano <undefined>
2024-09-30 11:07:13 -0300

    ✨ Add subtraction feature

--- calculator.py
+++ calculator.py
@@ -1,10 +1,13 @@
 import sys
 from src.add import add
+from src.subtraction import subtraction
 
 if __name__ == '__main__':
     a, op, b = sys.argv[1].split(' ')
     result = ''
     if op == '+':
         result = add(a, b)
+    elif op == '-':
+        result = subtraction(a, b)
     
     print(result)
